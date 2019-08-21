aa78ce088b94a622bc7199ed1fb9ea4af6689946
dcalleg707 <52968645+dcalleg707@users.noreply.github.com>
2019-08-20 23:03:18 -0500

    Update Day 10: Binary Numbers.py

--- 30 Days of Code/Day 10: Binary Numbers.py
+++ 30 Days of Code/Day 10: Binary Numbers.py
@@ -1,1 +1,29 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    nums = []
+    while n>0:
+        nums.append(n%2)
+        n = n //2
+    cont = 0
+    sol = 0
+    for i in nums:
+        
+        if i == 1:
+            cont+= 1
+            if cont > sol:
+                sol = cont
+        else:
+            cont = 0
+    print sol
+
 
