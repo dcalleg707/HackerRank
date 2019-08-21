fb46f5a0123d9b573d144200baf9274f51baab93
dcalleg707 <52968645+dcalleg707@users.noreply.github.com>
2019-08-20 23:22:03 -0500

    Create Day 17: More Exceptions.py

---
+++ 30 Days of Code/Day 17: More Exceptions.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def power(self,x,y):
+        if x < 0 or y < 0:
+            raise Exception("n and p should be non-negative") 
+        else:
+            return pow(x,y)
+
+myCalculator=Calculator()
+T=int(raw_input())
+for i in range(T):
+    n,p = map(int, raw_input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print ans
+    except Exception,e:
+        print e   
